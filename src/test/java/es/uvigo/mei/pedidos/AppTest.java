b6cb5bc5c7fe5a03290231e6009f9d4619ba665a
MEI-2018 SI <esei.mei2018.si@gmail.com>
2019-10-22 16:59:01 +0200

    Commit inicial

---
+++ src/test/java/es/uvigo/mei/pedidos/AppTest.java
@@ -0,0 +1,20 @@
+package es.uvigo.mei.pedidos;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
